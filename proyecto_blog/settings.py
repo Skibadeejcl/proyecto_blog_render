ea362030f805e6fb7a6fc5a1c75fa2503ed3fe88
Eduardo Castillo <eduardo.castillo.lara@outlook.com>
2025-01-31 20:42:54 -0300

    vamoh

--- proyecto_blog/settings.py
+++ proyecto_blog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1_@f6sdm%-lt(bg=wo=l-*rh*2c+7@0pk(56km(0yg)u+s#p9&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['proyecto-bloghs-render.onrender.com']
 
